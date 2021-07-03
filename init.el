8d789ace0774210c168a14df841b07703a091490
CSRaghunandan <rnraghunandan@gmail.com>
2021-07-03 13:58:56 +0530

    disable icons for dired and disable ligatures
    enable eshell module

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
        hl-todo                ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides     ; highlighted indent columns
-       ligatures ; ligatures and symbols to make your code pretty again
+       ;;ligatures ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline    ; snazzy, Atom-inspired modeline, plus API
        nav-flash   ; blink cursor line after big motions
@@ -66,14 +66,14 @@
        word-wrap                      ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons)    ; making dired pretty [functional]
+       dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        (ibuffer +icons)  ; interactive buffer management
        (undo +tree)      ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       ;;eshell            ; the elisp shell that works everywhere
+       eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        vterm                            ; the best terminal emulation in Emacs
